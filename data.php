fb993fe64ced678353e67f01710ed88b2e7fad75
mohamed amr <dev.mohamedamr@gmail.com>
2020-12-06 12:13:14 +0200

    add

---
+++ data.php
@@ -0,0 +1,161 @@
+<?php
+//  username from database
+// $username = "ahmed";
+// if($username == "ahmed")
+// {
+//     echo $username;
+// }else{
+//     echo 'error one';
+// }
+
+// if($username == "ahmed")
+// {
+//     echo $username;
+// }else{
+//     echo 'error two';
+// }
+
+// if($username == 'ahmed'){
+//     echo 'ahmed';
+// }elseif($username == 'mohamed'){
+//     echo 'mohamed';
+// }else{
+//     echo 'error';
+// }
+
+// if($username == 'ahmed'):
+//     echo 'ahmed';
+// elseif($username == 'mohamed'):
+//     echo 'mohamed';
+// else:
+//     echo 'error';
+// endif;
+    
+
+// $x =  $_POST['x'];
+// $y =  $_POST['y'];
+
+// echo $x + $y;
+
+
+// define("database_username","mohamed");
+// define("database_password","123456");
+
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+
+// if($username == database_username AND $password == database_password){
+//     echo 'success';
+// }else{
+//     echo 'error';
+// }
+
+// $name = 'mohamed';
+
+// echo "my name is $name <br>";
+// echo 'my name is $name <br>';
+// echo "my name is $name <br>";
+
+
+
+
+// if(20>10 and 30<20){
+//     echo 'true';
+// }else{
+//     echo 'false';
+// }
+
+// if(true && false){
+//     echo 'true';
+// }else{
+//     echo 'false';
+// }
+
+// if(true and false){
+//     echo 'true';
+// }else{
+//     echo 'false';
+// }
+
+
+
+
+// if(20>10 && 30<20){
+//     echo 'true';
+// }else{
+//     echo 'false';
+// }
+
+
+// $name = "mohamed";
+// $n = false and true;
+// $w = true && false;
+
+// echo ($n) ? 'success' : 'error';
+
+// $l = $_POST['l'];
+// $oil = $_POST['oil'];
+
+
+// if($oil == '92'){
+//     echo $l * 5;
+// }elseif($oil == '90'){
+//     echo $l * 2.5;
+// }elseif($oil == '80'){
+//     echo $l * 1;
+// }else{
+//     echo 'انت عبيط';
+// }
+
+// switch($oil){
+//     case 90:
+//         echo $l * 2;
+//     break;    
+//     case 90:
+//          echo $l * 10;
+//     break; 
+//     case 90:
+//          echo $l * 3;
+//     break;
+// }
+
+// echo "<table border='1'>";
+// for($y = 1;$y<=8;$y++){
+//     echo "<tr>";
+//     for($x=1;$x<=8;$x++){
+//         echo "<td>".$x."</td>";
+//     }
+//     echo "</tr>";
+// }
+// echo "</table>";
+
+
+// for($c = 0;$c<=15;$c++){
+//     echo "<h1 style='color:red'>$c</h1>";
+// }
+
+
+
+// $n = 10;
+// $n = $n++ + 20;
+// // echo $n;
+// echo ++$n - 2;
+// $n + 3;
+// $n-- + 10;
+// echo $n;
+// echo ++$n;
+// $n++;
+// echo $n;
+
+
+
+// echo "<style>
+//     .s{
+//         color:red;
+//     }
+// </style>";
+// echo "<h1 class='s'>ahmed</h1>";
+
+
+
+
